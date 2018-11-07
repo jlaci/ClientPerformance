200526b70bf7ef5be9c2b1740a7b59038a9a71d4
Jánoky László Viktor <janoky.laszlo@autsoft.hu>
2018-11-07 10:26:40 +0100

    Predictor with statistical weights

--- analyze/debug.py
+++ analyze/debug.py
@@ -24,19 +24,62 @@ for history_length in range(min_history + 1, max_history + 2):
         measurement_data = raw_data[:, measurement]
         for value_to_predict in range(history_length, len(measurement_data)):
             history_data.append(measurement_data[value_to_predict - history_length:value_to_predict])
+    # Shuffle the data points (vectors, not individual measurements!)
+    random.shuffle(history_data)
     data.append(history_data)
 
 
-# Learning
+# Linear predictor with calculated weights
+def autocovariance(data_vector, x):
+    n = len(data_vector)
+    avg = np.mean(data_vector)
+
+    sum = 0
+    for k in range(x, n):
+        sum += (data_vector[k] - avg) * (data_vector[k - x] - avg)
+
+    return 1/n * sum
+
+
+def autocorrellation(data_vector, x):
+    return autocovariance(data_vector, x) / autocovariance(data_vector, 0)
+
+
+def calculate_weights(data_matrix):
+    average_weights = [0] * len(data_matrix[0])
+
+    for data_vector in data_matrix:
+        n = len(data_vector)
+        r_matrix = [[0] * n for i in range(n)]
+        r_vector = [0] * n
+        for i in range(n):
+            for j in range(n):
+                r_matrix[i][j] = autocorrellation(data_vector, np.abs(j - i))
+            r_vector[i] = autocorrellation(data_vector, i + 1)
+
+        weights = np.linalg.solve(r_matrix, r_vector)
+
+        for i in range(n):
+            average_weights[i] += weights[i]
+
+    for i in range(len(data_matrix[0])):
+        average_weights[i] = average_weights[i] / len(data_matrix)
+
+    return average_weights
+
+# Learning with Linear Regression
 training_percentage = 0.5
 
 for data_set in data:
-    random.shuffle(data_set)
     cutting_point = math.floor(len(data_set) * training_percentage)
     training_set = np.asarray(data_set[:cutting_point])
     validation_set = np.asarray(data_set[cutting_point:])
-
     value_column = len(training_set[0]) - 1
+
+    # Linear predictor with statistical weights
+    w = calculate_weights(training_set)
+
+    # Linear Regression model
     model = linear_model.LinearRegression()
     model.fit(training_set[:, list(range(0, value_column))], training_set[:, value_column])
     prediction = model.predict(validation_set[:, list(range(0, value_column))])
