d9f674077e85f2700a2c8e6e39d09f78371fe8be
Jánoky László Viktor <jlv@nextval.hu>
2020-11-23 21:31:49 +0100

    New overhead calculating function

--- analyze/time_window.py
+++ analyze/time_window.py
@@ -2,16 +2,21 @@ import numpy as np
 import math
 
 raw_data = np.genfromtxt('data/time-window/tw1.csv', delimiter=";", dtype=float, encoding="utf-8-sig")
-original_delta = 100
+original_delta = 100    # Original time window used in the measurement file
 
 number_of_measurements = len(raw_data[0])
 
 ##########################################################################################
 # Global parameters
-known_history = 10  # The number of previous observed values
-delta_min = 100
-delta_step = 100
-delta_max = 10000
+known_history = 10      # The number of previous observed values used for prefiction
+delta_min = 100         # Minimum time window size
+delta_step = 100        # Increment of time window size
+delta_max = 1000        # Maximum of the time window size
+
+# Optimalization parameters
+alpha_min = 0
+alpha_max = 100
+alpha_step = 1
 
 # Measurement overhead (% of the total client performance contribution)
 network_time = 100  # The time it takes to upload the result to the server
@@ -75,19 +80,26 @@ def convert_data_set(data_set, original_delta, delta):
 # Returns the measurement overhead for the given delta. The overhead is the time when a client measures and sends the
 # data instead of calculating. Here we calculate the lost performance per second
 def measurement_overhead(delta, data_set):
+    # Calculate the average performance / second
     performance_per_second = np.average(data_set) * (1000 / original_delta)
     measurement_per_second = 1000 / delta
-    lost_performance = measurement_per_second * performance_per_second
-    max = np.amax(data_set)
+
+    # The cost of a single mesasurement in seconds
+    measurement_cost = (processing_time + prediction_time) / 1000
+
+    # The total number of measurements is total length (in seconds) * measurement / second * measurement_cost
+    total_measurement_time = ((len(data_set) * original_delta) / 1000) * measurement_per_second * measurement_cost
+
+    # Lost performance is the amount of measurements
+    lost_performance = total_measurement_time * performance_per_second
+    max = np.amax(data_set) * (1000 / original_delta)
     # Normalized lost performance
     return lost_performance / max
 
 # Mérések lefuttatása
-best_delta = delta_min
-best_delta_value = -1
-
 best_overall_delta = delta_min
 best_overall_delta_value = -1
+best_overall_alpha_value = -1.0
 
 for delta in range(delta_min, delta_max, delta_step):
     remaining_error = 0
@@ -109,20 +121,19 @@ for delta in range(delta_min, delta_max, delta_step):
         remaining_error += math.fabs(r_0 - rRr)
         total_overhead += overhead
 
-    # Calculate the best delta
+    # Calculate the best delta for each measurement
     avg_remaining_error = remaining_error / len(data_sets)
-    if best_delta_value == -1 or avg_remaining_error < best_delta_value:
-        best_delta_value = avg_remaining_error
-        best_delta = delta
-
-    # Calculate the best delta with measurement cost
     avg_overhead = total_overhead / len(data_sets)
-    weighted_cost = avg_remaining_error + avg_overhead
-    if best_overall_delta_value == -1 or weighted_cost < best_overall_delta_value:
-        best_overall_delta_value = weighted_cost
-        best_overall_delta = delta
 
-    print('Delta', delta, 'avg error:', avg_remaining_error, 'overhead: ',  avg_overhead, 'weighted error: ', weighted_cost)
+    for alpha in range(alpha_min, alpha_max, alpha_step):
+        weighted_cost = (1 - alpha/alpha_max) * avg_remaining_error + (alpha/alpha_max) * avg_overhead
+        if best_overall_delta_value == -1 or weighted_cost < best_overall_delta_value:
+            best_overall_delta_value = weighted_cost
+            best_overall_delta = delta
+            best_overall_alpha_value = alpha / alpha_max
+        #print('Delta', delta, 'avg error:', avg_remaining_error, 'overhead: ',  avg_overhead, 'weighted error: ', weighted_cost, 'alpha: ', alpha/alpha_max)
+        print(delta/1000, alpha/alpha_max, weighted_cost)
 
-print('Best delta', best_delta, ':', best_delta_value)
+
 print('Weighted best delta', best_overall_delta, ':', best_overall_delta_value)
+print('Weighted best delta', best_overall_delta, ':', best_overall_delta_value, ' with alpha: ', best_overall_alpha_value)
